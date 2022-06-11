d8ff8be59ebc4e026d6a71decde5fcf96840e765
pratiksj <pratiksharai728@gmail.com>
2022-06-11 17:50:48 +0545

    remaining array 1

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+//myReverse([1, 2, 3]);    // => [3, 2, 1]
+
+function myReverse(array) {
+  let newArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    let currVal = array[i];
+    newArray.push(currVal);
+  }
+
+  return newArray;
+}
+// let newArray = myReverse([1, 2, 3]); // => [3, 2, 1]
+// console.log(newArray);
--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+// unShift([1, 2, 3], 0); / => [0, 1, 2, 3];
+function myUnshift(array, value) {
+  let newArray = [value];
+  for (let i = 0; i < array.length; i++) {
+    let currVal = array[i];
+    newArray.push(currVal);
+  }
+  return newArray;
+}
